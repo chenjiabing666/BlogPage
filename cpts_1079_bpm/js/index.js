ccd4269837af76adb6786c1884ccb544ddd77ba7
chenjiabing666 <18796327106@163.com>
2018-08-17 18:51:19 +0800

    Blog

---
+++ cpts_1079_bpm/js/index.js
@@ -0,0 +1,4 @@
+var app = angular.module('index', []);
+app.controller('navCtrl', function($scope) {
+    // $scope.
+});
---
+++ cpts_1079_bpm/js/my.js
@@ -0,0 +1,8 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.click=function(){
+        $scope.firstName="chenjiabing";
+    }
+});
